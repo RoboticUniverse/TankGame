5fdc77caf19c9fd7cf1d3c93fd47bf5b0531039c
GrantJoslyn208 <gjjoslyn@gmail.com>
2023-05-02 12:41:50 -0600

    git commit

--- bullet.py
+++ bullet.py
@@ -20,14 +20,12 @@ class Bullet(pygame.sprite.Sprite):
             if sprite.rect.colliderect(self.rect):
                 if self.dx > 0:
                     self.rect.right = sprite.rect.left
-                    # self.x -= 1
-                    # self.rect.x = self.x - self.rect.width / 2
                 else:
                     self.rect.left = sprite.rect.right
-                    # self.x += 1
-                    # self.rect.x = self.x - self.rect.width / 2
-                print("xhit")
                 self.dx = -1 * self.dx
+                self.collisions_left -= 1
+                if self.collisions_left < 0:
+                    self.kill()
 
     def move_y(self, time_passed, walls):
         self.y += self.dy * time_passed * self.bullet_speed
@@ -36,15 +34,12 @@ class Bullet(pygame.sprite.Sprite):
             if sprite.rect.colliderect(self.rect):
                 if self.dy > 0:
                     self.rect.bottom = sprite.rect.top
-                    # self.y -= 1
-                    # self.rect.y = self.y - self.rect.width / 2
                 else:
                     self.rect.top = sprite.rect.bottom
-                    # self.y += 1
-                    # self.rect.y = self.y - self.rect.width / 2
-                print("yhit")
                 self.dy = -1 * self.dy
-
+                self.collisions_left -= 1
+                if self.collisions_left < 0:
+                    self.kill()
 
     def update(self, time_passed, walls):
         self.move_x(time_passed, walls)
